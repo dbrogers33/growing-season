dce5e86e69679c6b5db46b94fd352c7055c9a78b
Denham Rogers <denhamrogers10@gmail.com>
2020-03-19 19:29:36 -0500

    added social links to bio

--- src/components/about-sarah.js
+++ src/components/about-sarah.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import H2 from "../components/typography/h2"
 import H3 from "../components/typography/h3"
 import P from "../components/typography/p"
+import Social from "../components/social-button"
 import Container from "../components/container"
 import { StaticQuery, graphql } from 'gatsby'
 import Img from "gatsby-image/withIEPolyfill"
@@ -20,13 +21,26 @@ export default () => (
                     }
                 }
             }
-            
+            vine: file(relativePath: { eq: "corner-vine.png" }) {
+                childImageSharp {
+                    # Specify the image processing specifications right in the query.
+                    # Makes it trivial to update as your page's design changes.
+                    fixed(width: 100, height: 100) {
+                      ...GatsbyImageSharpFixed
+                    }
+                }
+            }
         }
       `}
       render={data => (
         <section>
             <Container>
                 <Content>
+                <Img
+                    fixed={data.vine.childImageSharp.fixed}
+                    alt='The Growing Season by Sarah Frey'
+                    style={{ position: "absolute", right: "0", top: "0", transform: "rotate(270deg)" }}
+                />
                     <Flexitem>
                     <Img
                         fluid={data.sarah.childImageSharp.fluid}
@@ -41,6 +55,7 @@ export default () => (
                         <Copy>
                             <H3>ABOUT THE AUTHOR</H3>
                             <H2>Meet Sarah</H2>
+                            <Social/>
                             <P>Sarah Frey has been described by the New York Times as “the Pumpkin Queen of America”. She sells more pumpkins than any other producer in the United States. Her family business, Frey Farms, plants thousands of acres of fruits and vegetables in Florida, Georgia, Missouri, Arkansas, Illinois, Indiana, and West Virginia. With a mission to end food waste in the fresh produce industry, the family makes natural food products and beverages from imperfect or “ugly fruit”.  Inspired by her humble beginnings and early life on the farm, she continues to create opportunities for those living and working in rural communities. Frey lives in Southern Illinois and is raising her two sons, William and Luke, on the same family farm where she grew up..</P>
                         </Copy>
                     </Flexitem>
@@ -56,6 +71,7 @@ export default () => (
 const Content = styled.div`
     background: #F7FCF9;
     margin: 4em 0 4em  0;
+    position: relative;
     @media (min-width: 1200px) {
         display: flex;
         align-items: stretch;
---
+++ src/components/social-button.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import styled from "styled-components";
+import { FaInstagram, FaFacebookF } from 'react-icons/fa';
+import P from '../components/typography/p'
+
+const Container = ({ }) => {
+    return (
+        <div>
+            <Link href="https://www.instagram.com/sara__frey/" >
+                <SocialButton>
+                        <FaInstagram style={{ fontSize: "18px", color: "rgba(0, 0, 0, 0.55)"}}/>
+                        <AccountName>@Sarah__Frey</AccountName>
+                </SocialButton>
+            </Link>
+
+            <Link href="https://www.facebook.com/sarah.talley.39" >
+                <SocialButton>
+                        <FaFacebookF style={{ fontSize: "18px", color: "rgba(0, 0, 0, 0.55)"}}/>
+                        <AccountName>@SarahFrey</AccountName>
+                </SocialButton>
+            </Link>
+        </div>
+    );
+};
+
+const SocialButton = styled.div`
+    border: 1px solid rgba(0, 0, 0, 0.55);
+    display: grid;
+    grid-template-columns: 1em auto;
+    padding: 0.75em 1em;
+    margin-top: .5em;
+`
+const AccountName = styled.p`
+    font-family: 'Brandon Grotesque Regular';
+    color: #545454;
+    font-size: 16px;
+    margin: 0;
+    padding: 0 0 0 .5em;
+`
+const Link = styled.a`
+    color: 1px solid rgba(0, 0, 0, 0.55);
+    text-decoration: none;
+    display: inline-block;
+    margin:  0 1em 0 0;
+    &:hover {
+        color: 1px solid rgba(0, 0, 0, 0.55);
+    }
+    &:visited {
+        color: 1px solid rgba(0, 0, 0, 0.55);
+    }
+`
+
+
+
+export default Container;
